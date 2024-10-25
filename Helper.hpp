4ea53ec92cf479e47608ccbf87d74b49c1b66857
Abrar200 <59603112+Abrar200@users.noreply.github.com>
2024-10-25 15:46:17 +1030

    Update Helper.hpp

--- Helper.hpp
+++ Helper.hpp
@@ -1,22 +1,20 @@
-// Helper.hpp
 #ifndef HELPER_HPP
 #define HELPER_HPP
 
 #include <tuple>
+#include <cstdlib> // for abs()
 
 class Helper {
 public:
-    static int absoluteSquaredDistance(std::tuple<int, int> item1, std::tuple<int, int> item2) {
+    static double absoluteSquaredDistance(std::tuple<int, int> item1, std::tuple<int, int> item2) {
         int x1 = std::get<0>(item1);
         int y1 = std::get<1>(item1);
         int x2 = std::get<0>(item2);
         int y2 = std::get<1>(item2);
         
-        int dx = x2 - x1;
-        int dy = y2 - y1;
-        
-        // Return integer squared distance
-        return (dx * dx) + (dy * dy);
+        // Following the formula: (absolute(x2 - x1) + absolute(y2 - y1))²
+        double diff = std::abs(x2 - x1) + std::abs(y2 - y1);
+        return diff * diff;
     }
 };
 
