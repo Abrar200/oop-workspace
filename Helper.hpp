6cd68fab23845f367edccb8d32f12ac133c224e4
Abrar200 <59603112+Abrar200@users.noreply.github.com>
2024-10-25 15:32:19 +1030

    Update Helper.hpp

--- Helper.hpp
+++ Helper.hpp
@@ -4,17 +4,19 @@
 #include <tuple>
 #include <cmath>
 
-class Helper
-{
+class Helper {
 public:
-    static double absoluteSquaredDistance(std::tuple<int, int> item1, std::tuple<int, int> item2)
-    {
+    static double absoluteSquaredDistance(std::tuple<int, int> item1, std::tuple<int, int> item2) {
         int x1 = std::get<0>(item1);
         int y1 = std::get<1>(item1);
         int x2 = std::get<0>(item2);
         int y2 = std::get<1>(item2);
-
-        return std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2);
+        
+        // Calculate absolute squared distance without using pow
+        // The formula is (x2-x1)² + (y2-y1)²
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        return dx * dx + dy * dy;
     }
 };
 
